4004a4159b8caf46a68c60c3412163153b7659c3
YoussefRs <rs.youssef24@gmail.com>
2023-07-06 01:07:13 +0100

    fix

---
+++ ievent_server/controllers/userDashboard.js
@@ -0,0 +1,18 @@
+const User = require("../models/user");
+
+const userDetails = async (req, res) => {
+    user_id = req.body.user_token;
+
+    User.find({ user_token: user_id }, async function (err, docs) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(200).send(docs[0]);
+            console.log(docs[0])
+        }
+    });
+};
+
+module.exports = {
+    userDetails,
+};
